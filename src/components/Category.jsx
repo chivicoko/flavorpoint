3c59a2abd52b6428d5f8b8420255a432bcdd8341
chivicoko <vicharmonic@gmail.com>
2023-01-19 11:49:06 +0000

    styled the nav in CategoryLink component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,28 +2,29 @@ import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
 import { GiNoodles, GiChopsticks } from "react-icons/gi";
 import { NavLink } from "react-router-dom";
 import List from "./List";
+import CategoryLink from "./CategoryLink";
 
 import React from 'react';
 
 function Category() {
   return (
     <List>
-      <NavLink to={'/cuisine/Italian'}>
+      <CategoryLink to={'/cuisine/Italian'}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/American'}>
+      </CategoryLink>
+      <CategoryLink to={'/cuisine/American'}>
         <FaHamburger />
         <h4>American</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/Thai'}>
+      </CategoryLink>
+      <CategoryLink to={'/cuisine/Thai'}>
         <GiNoodles />
         <h4>Thai</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/Japanese'}>
+      </CategoryLink>
+      <CategoryLink to={'/cuisine/Japanese'}>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </NavLink>
+      </CategoryLink>
     </List>
   )
 }
